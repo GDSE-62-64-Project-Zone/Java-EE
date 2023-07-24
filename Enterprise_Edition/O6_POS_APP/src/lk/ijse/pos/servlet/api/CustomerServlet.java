91c97b84b504f85e589bc529a2ea8d85132684fc
sanuv9683 <sanuv9683@gmail.com>
2023-07-24 09:36:08 +0530

    Path issue resolved

--- Enterprise_Edition/O6_POS_APP/src/lk/ijse/pos/servlet/api/CustomerServlet.java
+++ Enterprise_Edition/O6_POS_APP/src/lk/ijse/pos/servlet/api/CustomerServlet.java
@@ -7,11 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/customer")
+//http://localhost:8080/pos_one/customer
+//http://localhost:8080/pos_one/pages/customer? 404
+//http://localhost:8080/customer? 404
+
+//http://localhost:8080/pos_one/pages/customer//
+//http:://localhost:8080/pos_one/pages/customer
+//http:://localhost:8080/pos_one/pages/customer
+
+@WebServlet(urlPatterns = "/pages/customer")
 public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        System.out.println("Request Received");
     }
 }
