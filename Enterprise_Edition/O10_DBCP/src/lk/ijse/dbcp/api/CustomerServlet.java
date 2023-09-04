c5072e27b360442409c2463c201cbafee35a83f7
sanuv9683 <sanuv9683@gmail.com>
2023-09-04 15:14:41 +0530

    Content Updated with DBCP pool

--- Enterprise_Edition/O10_DBCP/src/lk/ijse/dbcp/api/CustomerServlet.java
+++ Enterprise_Edition/O10_DBCP/src/lk/ijse/dbcp/api/CustomerServlet.java
@@ -1,6 +1,7 @@
 package lk.ijse.dbcp.api;
 
 import lk.ijse.dbcp.api.util.ResponseUtil;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
@@ -11,7 +12,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
@@ -20,11 +24,21 @@ public class CustomerServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             //Initializing connection
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "sanu1234");
+            BasicDataSource pool = new BasicDataSource();
+            pool.setDriverClassName("com.mysql.jdbc.Driver");
+            pool.setUrl("jdbc:mysql://localhost:3306/company");
+            pool.setUsername("root");
+            pool.setPassword("sanu1234");
+            pool.setInitialSize(3);
+            pool.setMaxTotal(3);
+
+
+            Connection connection = pool.getConnection();
+
+
             PreparedStatement pstm = connection.prepareStatement("select * from Customer");
             ResultSet rst = pstm.executeQuery();
-            resp.addHeader("Content-Type","application/json");
+            resp.addHeader("Content-Type", "application/json");
 
             JsonArrayBuilder allCustomers = Json.createArrayBuilder();
             while (rst.next()) {
@@ -42,9 +56,6 @@ public class CustomerServlet extends HttpServlet {
             }
             //create the response Object
             resp.getWriter().print(ResponseUtil.genJson("Success", "Loaded", allCustomers.build()));
-        } catch (ClassNotFoundException e) {
-            resp.setStatus(500);
-            resp.getWriter().print(ResponseUtil.genJson("Error", e.getMessage()));
         } catch (SQLException e) {
             resp.setStatus(500);
             resp.getWriter().print(ResponseUtil.genJson("Error", e.getMessage()));
